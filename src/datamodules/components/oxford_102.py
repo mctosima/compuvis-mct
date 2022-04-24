16096a99515fd5e89a6f89c42f2ffba9ad981c83
Martin <mctosima@gmail.com>
2022-04-24 22:04:02 +0800

    Adding new file

---
+++ src/datamodules/components/oxford_102.py
@@ -0,0 +1,19 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class Oxford102(Dataset):
+    def __init__(self,
+                 data_dir: str = "data/oxford-102-flower-pytorch/flower_data/"):
+        
+        super().__init__()
+        
+        self.data_dir = data_dir
+        self.train_dir = data_dir + "/train"
+        self.test_dir = data_dir + "/test"
+        self.valid_dir = data_dir + "/valid"
+        
+    
+    def __len__(self):
+        return len(self.list)
+        
