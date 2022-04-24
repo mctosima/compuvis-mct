ca8d1ea0cb351b912cb91369cf3e0cd4e1a41490
Martin <mctosima@gmail.com>
2022-04-24 22:06:04 +0800

    Pre-Commit Fix

--- mnist_with_lightning.py
+++ mnist_with_lightning.py
@@ -1,11 +1,10 @@
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-
-import pytorch_lightning as pl
 from pytorch_lightning import Trainer
-import torch.nn.functional as F
 
 input_size = 784
 hidden_size = 500
@@ -13,6 +12,7 @@ num_classes = 10
 num_epochs = 2
 batch_size = 100
 learning_rate = 0.001
+
 
 class NeuralNet(pl.LightningModule):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -27,33 +27,32 @@ class NeuralNet(pl.LightningModule):
         out = self.relu(out)
         out = self.fc2(out)
         return out
-    
+
     def training_step(self, batch, batch_idx):
         images, labels = batch
         images = images.view(-1, 28 * 28)
-        
+
         # forward pass
         outputs = self(images)
         loss = F.cross_entropy(outputs, labels)
         return loss
-    
+
     def train_dataloader(self):
-        train_dataset = torchvision.datasets.MNIST(root='./data',
-                                           train=True,
-                                           transform=transforms.ToTensor(),
-                                           download=True)
-        
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           num_workers=2,
-                                           batch_size=batch_size,
-                                           shuffle=True)
+        train_dataset = torchvision.datasets.MNIST(
+            root="./data", train=True, transform=transforms.ToTensor(), download=True
+        )
+
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset, num_workers=2, batch_size=batch_size, shuffle=True
+        )
         return train_loader
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
         return optimizer
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     model = NeuralNet(input_size, hidden_size, num_classes)
     trainer = Trainer(max_epochs=num_epochs)
     trainer.fit(model)
--- src/datamodules/components/oxford_102.py
+++ src/datamodules/components/oxford_102.py
@@ -3,17 +3,14 @@ from torch.utils.data import Dataset
 
 
 class Oxford102(Dataset):
-    def __init__(self,
-                 data_dir: str = "data/oxford-102-flower-pytorch/flower_data/"):
-        
+    def __init__(self, data_dir: str = "data/oxford-102-flower-pytorch/flower_data/"):
+
         super().__init__()
-        
+
         self.data_dir = data_dir
         self.train_dir = data_dir + "/train"
         self.test_dir = data_dir + "/test"
         self.valid_dir = data_dir + "/valid"
-        
-    
+
     def __len__(self):
         return len(self.list)
-        
