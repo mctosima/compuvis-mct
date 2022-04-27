3f374514346f82cfca404f62015925333206db26
Wayan Wiprayoga <iwawiwi@gmail.com>
2022-04-27 16:40:33 +0800

    fix flower experiment

--- src/models/flower102_cnnmct.py
+++ src/models/flower102_cnnmct.py
@@ -5,78 +5,117 @@ from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric
 from torchmetrics.classification.accuracy import Accuracy
 
-from src.models.components.convmct import ConvMCT
+from src.models.components.simple_dense_net import SimpleDenseNet
 
 
 class Flowers102CNNMCT(LightningModule):
+    """Example of LightningModule for MNIST classification.
+
+    A LightningModule organizes your PyTorch code into 5 sections:
+        - Computations (init).
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Optimizers (configure_optimizers)
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
     def __init__(
         self,
-        learning_rate=0.001,
-        loss_fn=torch.nn.CrossEntropyLoss,
-        weight_decay: float = 0.0,
+        net: torch.nn.Module,
+        lr: float = 0.001,
+        weight_decay: float = 0.0005,
     ):
-
         super().__init__()
-        self.net = ConvMCT()
-        self.save_hyperparameters()
 
+        # this line allows to access init params with 'self.hparams' attribute
+        # it also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = net
+
+        # loss function
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
         self.train_acc = Accuracy()
         self.val_acc = Accuracy()
         self.test_acc = Accuracy()
+
+        # for logging best so far validation accuracy
         self.val_acc_best = MaxMetric()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         return self.net(x)
 
-    def configure_optimizers(self):
-        optimz = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
-        return optimz
-
-    def step(self, batch):
+    def step(self, batch: Any):
         x, y = batch
         logits = self.forward(x)
-        print(logits.shape)
-        print(y.shape)
-        print(self.hparams.loss_fn)
-        loss = self.hparams.loss_fn(logits, y - 1)
-        y_hat = torch.argmax(logits, dim=1)
-        return loss, y_hat, y
+        loss = self.criterion(logits, y - 1)
+        preds = torch.argmax(logits, dim=1)
+        return loss, preds, y
 
-    def training_step(self, batch, batch_idx):
-        loss, y_hat, y = self.step(batch)
-        acc = self.train_acc(y_hat, y)
-        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
-        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
-        return {"loss": loss}
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
 
-    def training_epoch_end(self, outputs):
+        # log train metrics
+        acc = self.train_acc(preds, targets)
+        self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=True, on_epoch=True, prog_bar=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in `training_epoch_end()`` below
+        # remember to always return loss from `training_step()` or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def training_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
         pass
 
-    def validation_step(self, batch, batch_idx):
-        loss, y_hat, y = self.step(batch)
-        acc = self.val_acc(y_hat, y)
-        self.log("val_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
-        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
-        return {"loss": loss}
+    # def validation_step(self, batch: Any, batch_idx: int):
+    #     loss, preds, targets = self.step(batch)
 
-    def validation_epoch_end(self, outputs):
-        acc = self.val_acc.compute()
-        self.val_acc_best.update(acc)
-        self.log(
-            "val_acc_best", self.val_acc_best.compute(), on_step=True, on_epoch=True, prog_bar=True
-        )
+    #     # log val metrics
+    #     acc = self.val_acc(preds, targets)
+    #     self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+    #     self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
 
-    def test_step(self, batch, batch_idx):
-        loss, y_hat, y = self.step(batch)
-        acc = self.test_acc(y_hat, y)
-        self.log("test_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
-        self.log("test_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
-        return {"loss": loss}
+    #     return {"loss": loss, "preds": preds, "targets": targets}
 
-    def test_epoch_end(self, outputs):
+    # def validation_epoch_end(self, outputs: List[Any]):
+    #     acc = self.val_acc.compute()  # get val accuracy from current epoch
+    #     self.val_acc_best.update(acc)
+    #     self.log("val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log test metrics
+        acc = self.test_acc(preds, targets)
+        self.log("test/loss", loss, on_step=False, on_epoch=True)
+        self.log("test/acc", acc, on_step=False, on_epoch=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_epoch_end(self, outputs: List[Any]):
         pass
 
     def on_epoch_end(self):
+        # reset metrics at the end of every epoch
         self.train_acc.reset()
-        self.val_acc.reset()
         self.test_acc.reset()
+        self.val_acc.reset()
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        return torch.optim.Adam(
+            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
+        )
