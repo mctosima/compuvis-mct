7ff39d943786052554373d80330cdf50b60a9142
Martin <mctosima@gmail.com>
2022-05-27 12:07:56 +0800

    Delete unimportant stuff

--- cifar_with_alexnet.py
+++
@@ -1,7 +0,0 @@
-import pytorch_lightning as pl
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-from pytorch_lightning import Trainer
